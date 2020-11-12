aa5660c82538eca460a325aa49679bb04ce19d0f
caioricciuti <caioricciuti07@gmail.com>
2020-11-12 14:22:07 +0100

    'update'

--- simple_task/settings.py
+++ simple_task/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['35.180.118.60', 'simpletask.caioricciuti.com', 'localhost']
+ALLOWED_HOSTS = ['35.180.118.60', 'simpletask.caioricciuti.com']
 
 # Application definition
 
